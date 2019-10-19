b895ca9de0bbe6ce97b091ec0e83add6627ebd25
Alex Schapelt <al-schapelt@t-online.de>
2019-10-19 18:45:57 +0200

    1-4 done

---
+++ Alex/Chapter14_VMAPI/leak.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+int main(void) {
+	int *P = (int *) malloc(sizeof(int));
+	return 0;
+}
---
+++ Alex/Chapter14_VMAPI/null.c
@@ -0,0 +1,7 @@
+#include <stddef.h>
+int main(void) {
+	int *p = NULL;
+	int i = *p;
+
+	return 0;
+}
